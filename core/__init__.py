38a65342c31d2ca5c4b9358425c40faee2d2dfbb
eaglesjo <eaglesjo@gmail.com>
2026-09-20 12:00:24 +0900

    feat: expose VYRELON orchestration runtime

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,11 @@
 """VYRELON orchestration core."""
 
+from core.delegation import Delegation, DelegationEngine
+from core.orchestrator import OrchestrationResult, Orchestrator
 from core.registry import AIRegistry, AgentRegistry
 from core.routing import AIRouter, Assignment
 
-__all__ = ["AIRegistry", "AgentRegistry", "AIRouter", "Assignment"]
+__all__ = [
+    "AIRegistry", "AgentRegistry", "AIRouter", "Assignment",
+    "Delegation", "DelegationEngine", "OrchestrationResult", "Orchestrator",
+]
