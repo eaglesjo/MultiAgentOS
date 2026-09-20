9a61c444b42416428cca56b4a4785647117c5f46
eaglesjo <eaglesjo@gmail.com>
2026-09-20 12:10:56 +0900

    feat: add common profile roles

---
+++ profiles/common.py
@@ -0,0 +1,11 @@
+"""Common role definitions shared by technology profiles."""
+
+COMMON_ROLES = (
+    "planner",
+    "researcher",
+    "editor",
+    "executor",
+    "tester",
+    "debugger",
+    "reviewer",
+)
