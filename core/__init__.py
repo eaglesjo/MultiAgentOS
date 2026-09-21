4e58030a22a91213f151c3cdf34c8ab836d9e42b
eaglesjo <eaglesjo@gmail.com>
2026-09-21 17:42:38 +0900

    feat: export review context contract

--- core/__init__.py
+++ core/__init__.py
@@ -6,6 +6,7 @@ from core.chat_agent_router import ChatAgentAssignment, ChatAgentRouter, ChatAge
 from core.chat_session import ChatSession, ChatSessionStore
 from core.contracts.chat_agent import ChatAgentContract, ChatAgentProvider
 from core.contracts.execution import AgentExecutor, ResultReviewer, ResultVerifier, ReviewDecision
+from core.contracts.handoff import ReviewContext
 from core.contracts.model_runtime import ModelAdapter, ModelRequest, ModelResponse
 from core.contracts.profile import DetectionResult, ProfileSpec
 from core.contracts.runtime import ExecutionRequest, RuntimeExecutor
@@ -19,7 +20,7 @@ from core.routing import AIRouter, Assignment, RoutingStrategy
 
 __all__ = [
     "AIRegistry", "AgentRegistry", "ChatAgentAssignment", "ChatAgentBridge", "ChatAgentRouter", "ChatAgentRoutingStrategy", "ChatSession", "ChatSessionStore", "ChatAgentExecutionResult", "ChatAgentRequest", "ChatAgentResponse", "ChatAgentRegistry", "ChatAgentContract", "ChatAgentProvider", "AIRouter", "Assignment", "RoutingStrategy",
-    "AgentExecutor", "ResultVerifier", "ResultReviewer", "ReviewDecision",
+    "AgentExecutor", "ResultVerifier", "ResultReviewer", "ReviewDecision", "ReviewContext",
     "ModelAdapter", "ModelRequest", "ModelResponse",
     "ExecutionRequest", "RuntimeExecutor",
     "DetectionResult", "ProfileSpec",
